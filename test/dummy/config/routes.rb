99b7eb2a9ff64736025848c15ee0321bb0ed4c8b
zcotter@forwardfinancing.com <jaffecotter.z@gmail.com>
2017-03-17 17:18:46 -0400

    Service to filter routes into desired hash list

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   mount RailsRoutesApiEngine::Engine => "/rails_routes_api_engine"
+  resources :some do
+    member do
+      get :some_custom_action
+    end
+  end
+
+  resources :another do
+    member do
+      get :another_custom_action
+    end
+  end
 end
